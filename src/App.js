e22171a1dc822799f2a141a1d6c88d301465419a
Eliran Shemesh <eliran.shemesh@netomedia.com>
2020-12-01 10:38:42 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import World from './components/world';
 
 function App() {
   return (
-    <div className="App">
-      <World cols={ 42 } rows={ 300 } />
-    </div>
+    <div className="App" />
   );
 }
 
